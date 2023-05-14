c991b7d9707006dcbc325007758769f3ecdbe82e
yurina-minami <yurinatty373@gmail.com>
2023-05-14 18:59:04 +0900

    レスポンシブ

--- js/main.js
+++ js/main.js
@@ -3,4 +3,9 @@ $(function () {
       $(this).toggleClass("active");
       $("nav").toggleClass("active");
     });
+
+    $(".nav-links").click(function () {
+        $(".hamburger").toggleClass("active");
+        $("nav").toggleClass("active");
+      });
 });
